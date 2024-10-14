afe12badae942568b1b32d4bf835ab65101ccac1
Matheus Dias <69007499+TheusAzevedo@users.noreply.github.com>
2024-10-14 10:39:42 -0300

    Update CalcuAPI.py

--- CalcuAPI.py
+++ CalcuAPI.py
@@ -1,24 +1,24 @@
 import tkinter as tk
 
-class Calculadora:
+class Calculator:
     def __init__(self):
-        self.tela = tk.Tk()
-        self.tela.title('Calculadora')
-        self.tela.resizable(width=False, height=False)
+        self.window = tk.Tk()
+        self.window.title('Calculator')
+        self.window.resizable(width=False, height=False)
 
         self.result = ""
         self.WIDTH = 10
         self.HEIGHT = 3
-        self.fonte = 'Arial 14 bold'
+        self.font = 'Arial 14 bold'
         
-        self.resultado = tk.Label(self.tela)
+        self.result_display = tk.Label(self.window)
         
-        self.auxResultado = tk.StringVar()
-        self.auxResultado.set('0')
-        self.lbResultado = tk.Label(self.tela, textvariable=self.auxResultado, font=self.fonte)
-        self.lbResultado.grid(row=0, columnspan=4, pady=10)
+        self.auxResult = tk.StringVar()
+        self.auxResult.set('0')
+        self.lbResult = tk.Label(self.window, textvariable=self.auxResult, font=self.font)
+        self.lbResult.grid(row=0, columnspan=4, pady=10)
 
-        botoes = [
+        buttons = [
             '7', '8', '9', '/',
             '4', '5', '6', '*',
             '1', '2', '3', '-',
@@ -27,30 +27,30 @@ class Calculadora:
 
         row = 1
         col = 0
-        for botao in botoes:
-            tk.Button(self.tela, text=botao, width=self.WIDTH, height=self.HEIGHT, font=self.fonte, command=lambda valor=botao: self.atualizar(valor)).grid(row=row, column=col, padx=2, pady=2)
+        for button in buttons:
+            tk.Button(self.window, text=button, width=self.WIDTH, height=self.HEIGHT, font=self.font, command=lambda value=button: self.update(value)).grid(row=row, column=col, padx=2, pady=2)
             col += 1
             if col > 3:
                 col = 0
                 row += 1
         
-        self.tela.mainloop()
+        self.window.mainloop()
 
-    def atualizar(self, valor):
-        if valor == 'C':
+    def update(self, value):
+        if value == 'C':
             self.result = ""
-        elif valor == '=':
-            self.calcular()
+        elif value == '=':
+            self.calculate()
         else:
-            self.result += valor
-        self.auxResultado.set(self.result)
+            self.result += value
+        self.auxResult.set(self.result)
 
-    def calcular(self):
+    def calculate(self):
         try:
-            resultado = eval(self.result)
-            self.result = str(resultado)
+            result = eval(self.result)
+            self.result = str(result)
         except:
-            self.result = "Impossivel calcular"
-        self.auxResultado.set(self.result)
+            self.result = "Unable to calculate"
+        self.auxResult.set(self.result)
 
-Calculadora()
+Calculator()
